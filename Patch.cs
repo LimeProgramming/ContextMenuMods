828f619c8670463755110def3abc1e1bf849b2be
Lime <LimeProgramming64@gmail.com>
2024-11-30 00:51:17 +0000

    update
    Removing Left and right options.
    I don't want them, use the original mod if you want them

--- Patch.cs
+++ Patch.cs
@@ -8,36 +8,25 @@ namespace Context_Menu_Funnies
 {
     public class Patch : ResoniteMod
     {
-        public override string Author => "LeCloutPanda";
-        public override string Name => "Context Menu Funnies";
-        public override string Version => "1.1.0";
-        public override string Link => "https://github.com/LeCloutPanda/ContextMenuFunnies/";
+        public override string Author => "CalamityLime";
+        public override string Name => "Context Menu Mods";
+        public override string Version => "1.2.0";
+        public override string Link => "https://github.com/LimeProgramming/ContextMenuMods/";
 
         [AutoRegisterConfigKey] private static ModConfigurationKey<bool> MASTER_ENABLED = new ModConfigurationKey<bool>("Enabled", "", () => true);
         [AutoRegisterConfigKey] private static ModConfigurationKey<dummy> SPACER_A = new ModConfigurationKey<dummy>("", "");
         // Left
 
-        [AutoRegisterConfigKey] private static ModConfigurationKey<float> LEFT_SEPERATION = new ModConfigurationKey<float>("Left menu item seperation", "", () => 6f);
-        [AutoRegisterConfigKey] private static ModConfigurationKey<float> LEFT_RADIUS_RATIO = new ModConfigurationKey<float>("Left menu distance from center", "", () => 0.5f);
-        [AutoRegisterConfigKey] private static ModConfigurationKey<float> LEFT_ARCLAYOUT_ARC = new ModConfigurationKey<float>("Left menu arc amount", "", () => 360f);
-        [AutoRegisterConfigKey] private static ModConfigurationKey<float> LEFT_ARCLAYOUT_OFFSET = new ModConfigurationKey<float>("Left menu rotation offset", "", () => 0f);
-        [AutoRegisterConfigKey] private static ModConfigurationKey<ArcLayout.Direction> LEFT_ITEM_DIRECTION = new ModConfigurationKey<ArcLayout.Direction>("Left menu layout direction", "", () => ArcLayout.Direction.Clockwise);
-        [AutoRegisterConfigKey] private static ModConfigurationKey<bool> LEFT_ICON_ENABLED = new ModConfigurationKey<bool>("Left menu center icon enabled", "", () => true);
-        [AutoRegisterConfigKey] private static ModConfigurationKey<bool> LEFT_INNER_CIRCLE_ENABLED = new ModConfigurationKey<bool>("Left menu inner circle enabled", "", () => true);
-        [AutoRegisterConfigKey] private static ModConfigurationKey<float> LEFT_ROUNDED_CORNER_RADIUS = new ModConfigurationKey<float>("Left Rounded Corner Radius", "", () => 16f);
-        [AutoRegisterConfigKey] private static ModConfigurationKey<float> LEFT_FILL_COLOR_ALPHA = new ModConfigurationKey<float>("Left Fill Color Alpha", "", () => 1f);
+        [AutoRegisterConfigKey] private static ModConfigurationKey<float> CTX_SEPERATION = new ModConfigurationKey<float>("CTX menu item seperation", "", () => 6f);
+        [AutoRegisterConfigKey] private static ModConfigurationKey<float> CTX_RADIUS_RATIO = new ModConfigurationKey<float>("CTX menu distance from center", "", () => 0.5f);
+        [AutoRegisterConfigKey] private static ModConfigurationKey<float> CTX_ARCLAYOUT_ARC = new ModConfigurationKey<float>("CTX menu arc amount", "", () => 360f);
+        [AutoRegisterConfigKey] private static ModConfigurationKey<float> CTX_ARCLAYOUT_OFFSET = new ModConfigurationKey<float>("CTX menu rotation offset", "", () => 0f);
+        [AutoRegisterConfigKey] private static ModConfigurationKey<ArcLayout.Direction> CTX_ITEM_DIRECTION = new ModConfigurationKey<ArcLayout.Direction>("CTX menu layout direction", "", () => ArcLayout.Direction.Clockwise);
+        [AutoRegisterConfigKey] private static ModConfigurationKey<bool> CTX_ICON_ENABLED = new ModConfigurationKey<bool>("CTX menu center icon enabled", "", () => true);
+        [AutoRegisterConfigKey] private static ModConfigurationKey<bool> CTX_INNER_CIRCLE_ENABLED = new ModConfigurationKey<bool>("CTX menu inner circle enabled", "", () => true);
+        [AutoRegisterConfigKey] private static ModConfigurationKey<float> CTX_ROUNDED_CORNER_RADIUS = new ModConfigurationKey<float>("CTX Rounded Corner Radius", "", () => 16f);
+        [AutoRegisterConfigKey] private static ModConfigurationKey<float> CTX_FILL_COLOR_ALPHA = new ModConfigurationKey<float>("CTX Fill Color Alpha", "", () => 1f);
 
-        [AutoRegisterConfigKey] private static ModConfigurationKey<dummy> SPACER_B = new ModConfigurationKey<dummy>(" ", "");
-        // Right
-        [AutoRegisterConfigKey] private static ModConfigurationKey<float> RIGHT_SEPERATION = new ModConfigurationKey<float>("Right menu item seperation", "", () => 6f);
-        [AutoRegisterConfigKey] private static ModConfigurationKey<float> RIGHT_RADIUS_RATIO = new ModConfigurationKey<float>("Right menu distance from center", "", () => 0.5f);
-        [AutoRegisterConfigKey] private static ModConfigurationKey<float> RIGHT_ARCLAYOUT_ARC = new ModConfigurationKey<float>("Right menu arc amount", "", () => 360f);
-        [AutoRegisterConfigKey] private static ModConfigurationKey<float> RIGHT_ARCLAYOUT_OFFSET = new ModConfigurationKey<float>("Right menu rotation offset", "", () => 0f);
-        [AutoRegisterConfigKey] private static ModConfigurationKey<ArcLayout.Direction> RIGHT_ITEM_DIRECTION = new ModConfigurationKey<ArcLayout.Direction>("Right menu layout direction", "", () => ArcLayout.Direction.Clockwise);
-        [AutoRegisterConfigKey] private static ModConfigurationKey<bool> RIGHT_ICON_ENABLED = new ModConfigurationKey<bool>("Right menu center icon enabled", "", () => true);
-        [AutoRegisterConfigKey] private static ModConfigurationKey<bool> RIGHT_INNER_CIRCLE_ENABLED = new ModConfigurationKey<bool>("Right menu inner circle enabled", "", () => true);
-        [AutoRegisterConfigKey] private static ModConfigurationKey<float> RIGHT_ROUNDED_CORNER_RADIUS = new ModConfigurationKey<float>("Right Rounded Corner Radius", "", () => 16f);
-        [AutoRegisterConfigKey] private static ModConfigurationKey<float> RIGHT_FILL_COLOR_ALPHA = new ModConfigurationKey<float>("Right Fill Color Alpha", "", () => 1f);
 
         private static ModConfiguration config;
 
@@ -63,23 +52,15 @@ namespace Context_Menu_Funnies
                     {
                         if (__instance.Slot.ActiveUserRoot.ActiveUser != __instance.LocalUser) return;
 
-                        Chirality side = __instance.Pointer.Target.GetComponent<InteractionLaser>().Side;
+                        //Chirality side = __instance.Pointer.Target.GetComponent<InteractionLaser>().Side;
 
-                        float seperation = side == Chirality.Right ? config.GetValue(RIGHT_SEPERATION) : config.GetValue(LEFT_SEPERATION);
-                        float radiusRatio = side == Chirality.Right ? config.GetValue(RIGHT_RADIUS_RATIO) : config.GetValue(LEFT_RADIUS_RATIO);
-                        float arc = side == Chirality.Right ? config.GetValue(RIGHT_ARCLAYOUT_ARC) : config.GetValue(LEFT_ARCLAYOUT_ARC);
-                        float offset = side == Chirality.Right ? config.GetValue(RIGHT_ARCLAYOUT_OFFSET) : config.GetValue(LEFT_ARCLAYOUT_OFFSET);
-                        ArcLayout.Direction direction = side == Chirality.Right ? config.GetValue(RIGHT_ITEM_DIRECTION) : config.GetValue(LEFT_ITEM_DIRECTION);
-                        bool innerCircleEnabled = side == Chirality.Right ? config.GetValue(RIGHT_INNER_CIRCLE_ENABLED) : config.GetValue(LEFT_INNER_CIRCLE_ENABLED);
-                        bool iconEnabled = side == Chirality.Right ? config.GetValue(RIGHT_ICON_ENABLED) : config.GetValue(LEFT_ICON_ENABLED);
-
-                        ___Separation.Value = seperation;
-                        ___RadiusRatio.Value = radiusRatio;
-                        ____arcLayout.Target.Arc.Value = arc;
-                        ____arcLayout.Target.ItemDirection.Value = direction;
-                        ____arcLayout.Target.Offset.Value = offset;
-                        ____innerCircle.Target.Enabled = innerCircleEnabled;
-                        ____iconImage.Target.Enabled = iconEnabled;
+                        ___Separation.Value =                       config.GetValue(CTX_SEPERATION);
+                        ___RadiusRatio.Value =                      config.GetValue(CTX_RADIUS_RATIO);
+                        ____arcLayout.Target.Arc.Value =            config.GetValue(CTX_ARCLAYOUT_ARC);
+                        ____arcLayout.Target.ItemDirection.Value =  config.GetValue(CTX_ITEM_DIRECTION);
+                        ____arcLayout.Target.Offset.Value =         config.GetValue(CTX_ARCLAYOUT_OFFSET);
+                        ____innerCircle.Target.Enabled =            config.GetValue(CTX_INNER_CIRCLE_ENABLED);
+                        ____iconImage.Target.Enabled =              config.GetValue(CTX_ICON_ENABLED);
                     });
                 }
             }
@@ -98,10 +79,12 @@ namespace Context_Menu_Funnies
                 if (activeUser == null || activeUser != __instance.LocalUser) return;
 
                 ContextMenu menu = __instance.Slot?.GetComponentInParents<ContextMenu>();
-                Chirality? side = menu?.Pointer?.Target?.GetComponent<InteractionLaser>()?.Side;
-                if (menu == null || side == null || menu.Slot.ActiveUserRoot.ActiveUser != __instance.LocalUser) return;
+                //Chirality? side = menu?.Pointer?.Target?.GetComponent<InteractionLaser>()?.Side;
+                //if (menu == null || side == null || menu.Slot.ActiveUserRoot.ActiveUser != __instance.LocalUser) return;
 
-                arc.RoundedCornerRadius.Value = side == Chirality.Right ? config.GetValue(RIGHT_ROUNDED_CORNER_RADIUS) : config.GetValue(LEFT_ROUNDED_CORNER_RADIUS);
+                if (menu == null || menu.Slot.ActiveUserRoot.ActiveUser != __instance.LocalUser) return;
+
+                arc.RoundedCornerRadius.Value = config.GetValue(CTX_ROUNDED_CORNER_RADIUS);
             }
 
             [HarmonyPostfix]
@@ -114,13 +97,15 @@ namespace Context_Menu_Funnies
                 if (activeUser == null || activeUser != __instance.LocalUser) return;
 
                 ContextMenu menu = __instance.Slot?.GetComponentInParents<ContextMenu>();
-                Chirality? side = menu?.Pointer?.Target?.GetComponent<InteractionLaser>()?.Side;
-                if (menu == null || side == null || menu.Slot.ActiveUserRoot.ActiveUser != __instance.LocalUser) return;
+                //Chirality? side = menu?.Pointer?.Target?.GetComponent<InteractionLaser>()?.Side;
+                //if (menu == null || side == null || menu.Slot.ActiveUserRoot.ActiveUser != __instance.LocalUser) return;
+
+                if (menu == null || menu.Slot.ActiveUserRoot.ActiveUser != __instance.LocalUser) return;
 
                 var colorDrive = ____button?.Target?.ColorDrivers[0];
                 if (colorDrive == null) return;
 
-                var alpha = side == Chirality.Right ? config.GetValue(RIGHT_FILL_COLOR_ALPHA) : config.GetValue(LEFT_FILL_COLOR_ALPHA);
+                var alpha = config.GetValue(CTX_FILL_COLOR_ALPHA);
 
                 var oldNormalColor = colorDrive.NormalColor.Value;
                 var oldHighlightColor = colorDrive.HighlightColor.Value;
